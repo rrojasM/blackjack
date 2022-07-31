b40995cde2cb1d8b84c23a5ce952ffef7787b3a3
Rufino Rojas <ruf_rm@hotmail.com>
2022-07-31 18:39:06 -0500

    DOM

--- js/juego.js
+++ js/juego.js
@@ -7,7 +7,27 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+let puntosJugador = 0;
+let puntosCoomputadora = 0;
 
+
+//REFERENCIAS HTML
+const btnPedir = document.querySelector('#btnPedir');
+const puntosHTML = document.querySelectorAll('small');
+
+
+function shuffle(arr) {
+    var i, j, temp;
+    for (i = arr.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;
+}
+
+/* Esta función crea una nueva baraja */
 const crearDeck = () => {
 
     for (let i = 2; i <= 10; i++) {
@@ -21,9 +41,62 @@ const crearDeck = () => {
             deck.push(esp + tipo);
         }
     }
-
-    console.log(deck);
-
+    deck = shuffle(deck)
+    return deck;
 }
 
 crearDeck();
+/* Esta función me permite tomar una carta */
+
+const pedirCarta = () => {
+    if (deck.length === 0) {
+        throw 'NO HAY MAS CARTAS EN EL DECK!!'
+    }
+    const carta = deck.pop();
+    return carta;
+}
+
+/*
+for (let i = 0 ; 1 <= 55; i++) {
+    pedirCarta()
+} */
+
+//pedirCarta();
+
+/* const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    let puntos = 0;
+
+    if (isNaN(valor)) {
+        //console.log('NO ES UN NÚMERO');
+        puntos = (valor === 'A') ? 11 : 10;
+        console.log(puntos);
+    } else {
+        //console.log('ES UN NÚMERO');
+        puntos = valor * 1;//* 1 retorna la versión numerica
+    }
+} */
+
+
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor)) ?
+        (valor === 'A') ? 11 : 10
+        : valor * 1;
+}
+
+
+
+
+
+btnPedir.addEventListener('click', () => {
+    const carta = pedirCarta();
+
+    puntosJugador = puntosJugador + valorCarta(carta);
+    //PRIMER SMALL
+    puntosHTML[0].innerText = puntosJugador;
+    console.log('PUNTOS DEL JUGADOR', puntosJugador);
+
+
+    
+});
